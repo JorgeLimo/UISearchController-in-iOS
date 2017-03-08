f55e9d7995e45f25eb5c4d2378de617b4b4f07a0
Alumno <alumno@admins-macbook-pro.local>
2017-03-07 20:57:50 -0500

    search controller

---
+++ UISearchController in iOS/Dispositivo.swift
@@ -0,0 +1,18 @@
+//
+//  Dispositivo.swift
+//  UISearchController in iOS
+//
+//  Created by Alumno on 7/03/17.
+//  Copyright © 2017 JorgeLimo. All rights reserved.
+//
+
+import UIKit
+
+class Dispositivo: NSObject {
+    
+    var nombre:String!
+    var precio:Float!
+    var fabricacion:String!
+    
+
+}
--- UISearchController in iOS/ViewController.swift
+++ UISearchController in iOS/ViewController.swift
@@ -8,12 +8,115 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UISearchResultsUpdating {
 
+    @IBOutlet weak var table: UITableView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var dispositivios = Array<Dispositivo>()
+    var dispositiviosFiltrados = Array<Dispositivo>()
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let texto = searchController.searchBar.text
+        
+        dispositiviosFiltrados = dispositivios.filter({ (dispositivo) -> Bool in
+            return String(dispositivo.precio).lowercased().contains(texto!.lowercased()) || dispositivo.fabricacion.lowercased().contains(texto!.lowercased()) || dispositivo.nombre.lowercased().contains(texto!.lowercased())
+        })
+        table.reloadData()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchController.searchResultsUpdater = self
+        table.tableHeaderView = searchController.searchBar
+        
+        for i in 1...9{
+            
+            let dispo = Dispositivo()
+         
+            switch i {
+            case 1:
+                dispo.nombre = "iPhone"
+                dispo.precio = 599.00
+                dispo.fabricacion = "2017"
+            case 2:
+                dispo.nombre = "iPod"
+                dispo.precio = 399.00
+                dispo.fabricacion = "2016"
+            case 3:
+                dispo.nombre = "iPod Touch"
+                dispo.precio = 210.00
+                dispo.fabricacion = "2017"
+            case 4:
+                dispo.nombre = "iPad"
+                dispo.precio = 499.00
+                dispo.fabricacion = "2015"
+            case 5:
+                dispo.nombre = "iPad Mini"
+                dispo.precio = 659.00
+                dispo.fabricacion = "2016"
+            case 6:
+                dispo.nombre = "Mac"
+                dispo.precio = 1299.00
+                dispo.fabricacion = "2015"
+            case 7:
+                dispo.nombre = "Mac Pro"
+                dispo.precio = 2499.00
+                dispo.fabricacion = "2007"
+            case 8:
+                dispo.nombre = "MacBook Air"
+                dispo.precio = 1799.00
+                dispo.fabricacion = "2007"
+            default:
+                dispo.nombre = "MacBook Pro"
+                dispo.precio = 1499.00
+                dispo.fabricacion = "2007"
+            }
+            
+            dispositivios.append(dispo)
+            
+        }
+        
+        
+        
     }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    
+        if  searchController.isActive && searchController.searchBar.text != "" {
+            return dispositiviosFiltrados.count
+        }
+        
+        return dispositivios.count
+    
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celda", for : indexPath)
+
+        let item:Dispositivo
+        
+        if  searchController.isActive && searchController.searchBar.text != "" {
+            item = dispositiviosFiltrados[indexPath.row]
+        }else{
+            item = dispositivios[indexPath.row]
+        }
+        
+ 
+        cell.textLabel?.text = item.nombre!
+        cell.detailTextLabel?.text = "$\(item.precio!) | \(item.fabricacion!)"
+        
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
